eab6b1669f9734b08cab2504eee5efe90d8f20c7
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-06-10 13:56:23 -0500

    wrapped main. using logging module.

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 #!/usr/bin/env python3
-from datetime import datetime
+import argparse
 import subprocess
 import time
 import waggle.plugin as plugin
+import logging
 
-def log(*args, **kwargs):
-    print(*args, **kwargs, flush=True)
 
-plugin.init()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rate", default=300, type=float, help="sampling interval in seconds")
+    args = parser.parse_args()
 
-log("sampler started. will sample every 5m")
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(message)s',
+        datefmt='%Y/%m/%d %H:%M:%S')
 
-while True:
-    time.sleep(300)
-    
-    log("recording raw audio sample")
-    try:
-        subprocess.check_call(["arecord", "-f", "cd", "-d", "30", "-r", "44100", "-c", "1", "sample.wav"])
-    except subprocess.CalledProcessError:
-        log("failed to record wav. will retry")
-        continue
+    plugin.init()
 
-    log("converting sample to mp3")
-    try:
-        subprocess.check_call(["ffmpeg", "-y", "-i", "sample.wav", "-ac", "1", "-acodec", "mp3", "-ab", "128k", "sample.mp3"])
-    except subprocess.CalledProcessError:
-        log("failed to convert to mp3. will retry")
-        continue
+    logging.info("sampler started. will sample every %ss", args.rate)
 
-    log("uploading sample")
-    plugin.upload_file("sample.mp3")
+    while True:
+        time.sleep(args.rate)
+        
+        logging.info("recording raw audio sample")
+        try:
+            subprocess.check_call(["arecord", "-f", "cd", "-d", "30", "-r", "44100", "-c", "1", "sample.wav"])
+        except subprocess.CalledProcessError:
+            logging.info("failed to record wav. will retry")
+            continue
+
+        logging.info("converting sample to mp3")
+        try:
+            subprocess.check_call(["ffmpeg", "-y", "-i", "sample.wav", "-ac", "1", "-acodec", "mp3", "-ab", "128k", "sample.mp3"])
+        except subprocess.CalledProcessError:
+            logging.info("failed to convert to mp3. will retry")
+            continue
+
+        logging.info("uploading sample")
+        plugin.upload_file("sample.mp3")
+
+
+if __name__ == "__main__":
+    main()
